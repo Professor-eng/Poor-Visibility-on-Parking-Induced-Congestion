ecec04d89b4ff1bf46b9c07c1aa0f4613ba1de63
Alokik Sharma <alokiksharm00.7@gmail.com>
2026-06-23 20:03:51 +0530

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,25 +1,28 @@
 import pandas as pd
 
 def feature_engineering(indexed_df_path: str) -> pd.DataFrame:
-    """
-    Transforms ticket-level data into a structured Spatio-Temporal Master Table.
-    """
     df = pd.read_csv(indexed_df_path)
     
     df['created_datetime'] = pd.to_datetime(df['created_datetime'], format='ISO8601', utc=True)
     df['created_datetime'] = df['created_datetime'].dt.tz_convert('Asia/Kolkata')
     df['hour'] = df['created_datetime'].dt.hour
     df['day_of_week'] = df['created_datetime'].dt.dayofweek
-    df['is_weekend'] = df['day_of_week'].apply(lambda x: 1 if x >= 5 else 0)
-    df['is_peak_hour'] = df['hour'].apply(lambda h: 1 if (8 <= h <= 12) or (16 <= h <= 20) else 0)
+    df["is_weekend"] = (df["day_of_week"] >= 5).astype(int)
     
-    master_df = df.groupby(['h3_cell', 'hour', 'day_of_week', 'is_weekend', 'is_peak_hour']).agg(
-        total_violations=('id', 'count'),
-        aggregated_severity=('severity_score', 'sum'),
-        mean_latitude=('latitude', 'mean'),
-        mean_longitude=('longitude', 'mean')
-    ).reset_index()   
-
+    df["is_peak_hour"] = (
+        ((df["hour"] >= 8) & (df["hour"] <= 12)) |
+        ((df["hour"] >= 16) & (df["hour"] <= 20))
+    ).astype(int)    
+    
+    master_df = df.groupby(
+        ["h3_cell", "hour", "day_of_week", "is_weekend", "is_peak_hour"]
+    ).agg({
+        "id": "count",
+        "severity_score": "sum",
+        "latitude": "mean",
+        "longitude": "mean"
+    }).reset_index()
+    
     severity_threshold = master_df['aggregated_severity'].quantile(0.75)
     master_df['is_hotspot'] = (master_df['aggregated_severity'] > severity_threshold).astype(int)
     
